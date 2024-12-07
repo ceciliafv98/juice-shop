b6657a4ee74f4805515b439a62965df39bfabcb9
ceciliafv98 <yzp14903@educastur.es>
2024-12-07 08:01:28 +0100

    Update vulnCodeSnippet.ts
    Se crea un path.join (línea 94) que incluye $key para evitar que se introduzca información maliciosa

--- routes/vulnCodeSnippet.ts
+++ routes/vulnCodeSnippet.ts
@@ -90,8 +90,13 @@ exports.checkVulnLines = () => async (req: Request<Record<string, unknown>, Reco
   const selectedLines: number[] = req.body.selectedLines
   const verdict = getVerdict(vulnLines, neutralLines, selectedLines)
   let hint
-  if (fs.existsSync('./data/static/codefixes/' + key + '.info.yml')) {
-    const codingChallengeInfos = yaml.load(fs.readFileSync('./data/static/codefixes/' + key + '.info.yml', 'utf8'))
+
+  const filePath = path.join(__dirname, 'data', 'static', 'codefixes', `${key}.info.yml`);
+  
+  //if (fs.existsSync('./data/static/codefixes/' + key + '.info.yml')) {
+  if (fs.existsSync(filePath)){
+    //const codingChallengeInfos = yaml.load(fs.readFileSync('./data/static/codefixes/' + key + '.info.yml', 'utf8'))
+    const codingChallengeInfos = yaml.load(fs.readFileSync(filePath, 'utf8'));
     if (codingChallengeInfos?.hints) {
       if (accuracy.getFindItAttempts(key) > codingChallengeInfos.hints.length) {
         if (vulnLines.length === 1) {
